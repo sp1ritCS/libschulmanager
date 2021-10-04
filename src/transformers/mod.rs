b8751a52bad569f25faea485782f45a5946ef0af
Florian "sp1rit"​ <sp1ritCS@protonmail.com>
2021-10-04 22:32:15 +0200

    Major request handling rework
    - Request are now mostly handled by sm::RequestManager
    - Support for multi query request
    - Minor API changes (update to version 0.2.1)
    - split sm_req into sm:: subtree
    - changed struct fields to snake_case (with serde rename camelCase)

--- src/transformers/mod.rs
+++ src/transformers/mod.rs
@@ -5,27 +5,15 @@ pub mod smartv2;
 
 impl crate::SmTimetable {
 	#[cfg(feature = "smartv1")]
-    pub fn to_smart_v1(self) -> Result<Vec<smartv1::SmWeek>, Box<dyn std::error::Error>> {
-        let mut timetables: Vec<smartv1::SmWeek> = vec![];
-        for timetable in self.interna_timetable.results {
-            timetables.push(smartv1::SmWeek::from_interna(timetable)?);
-        }
-        Ok(timetables)
+    pub fn to_smart_v1(self) -> Result<smartv1::SmWeek, Box<dyn std::error::Error>> {
+        smartv1::SmWeek::from_interna(self.interna_timetable)
     }
     #[cfg(feature = "smartv2")]
-    pub fn to_smart_v2_weekdays(self) -> Result<Vec<smartv2::Weekdays>, Box<dyn std::error::Error>> {
-        let mut timetables: Vec<smartv2::Weekdays> = vec![];
-        for timetable in self.interna_timetable.results {
-            timetables.push(smartv2::Weekdays::from_interna(timetable)?);
-        }
-        Ok(timetables)
+    pub fn to_smart_v2_weekdays(self) -> Result<smartv2::Weekdays, Box<dyn std::error::Error>> {
+        smartv2::Weekdays::from_interna(self.interna_timetable)
     }
     #[cfg(feature = "smartv2")]
-    pub fn to_smart_v2_daymap(self) -> Result<Vec<smartv2::DayMap>, Box<dyn std::error::Error>> {
-        let mut timetables: Vec<smartv2::DayMap> = vec![];
-        for timetable in self.interna_timetable.results {
-            timetables.push(smartv2::DayMap::from_interna(timetable)?);
-        }
-        Ok(timetables)
+    pub fn to_smart_v2_daymap(self) -> Result<smartv2::DayMap, Box<dyn std::error::Error>> {
+        smartv2::DayMap::from_interna(self.interna_timetable)
     }
 }
